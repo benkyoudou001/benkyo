09481c184eb0b8992af6fe6564828c6625470701
benkyoudou001 <nozaki@ameyokoyaku.co.jp>
2024-09-11 17:46:48 +0900

    2024.09.11 更新

--- js/banner_timer.js
+++ js/banner_timer.js
@@ -1,7 +1,7 @@
 var startday = [];
 var endday = [];
-startday[1] = new Date('2024/09/09 18:00:00');
-endday[1] = new Date('2024/09/10 23:59:59');
+startday[1] = new Date('2024/09/14 18:00:00');
+endday[1] = new Date('2024/09/15 23:59:59');
 var today = new Date();
 for (i = 1; i <= 2; i++) {
     if (startday[i] > today || today > endday[i]) {
